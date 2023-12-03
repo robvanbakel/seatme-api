83787357ee351634a1657112b28977ee6b121172
Rob van Bakel <robbakel@gmail.com>
2023-12-03 12:07:52 +0100

    Initialize Supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase credentials not set");
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
